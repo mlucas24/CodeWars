3d94b8aff3d28798a723c3714a935ca776026e83
mlucas24 <mlucas24@icloud.com>
2020-09-02 02:08:50 -0400

    accepted largestTime

--- javascript/largestTime/largestTime.js
+++ javascript/largestTime/largestTime.js
@@ -11,45 +11,56 @@
 // Output: ""
 
 var largestTimeFromDigits = function(A) {
-    let returnTime = "";
-    let time1 = "";
-    let time2 = "";
-    let time4 = "";
-    let time5 = "";
-
-    const isFail = () => {
-        if (time1.length === 0) return false;
-        if (time4.length === 0) return false;
-        if (time2.length === 0) return false;
-    }
-
-    for (let i = 0; i < 4; i++) {
-        if (A[i] <= 2) {
-            time1 += i;
-        }
-        if (A[i] <= 5) {
-            time4 += i;
-        }
-        time2 += i;
-        time5 += i;
-    }
-
-    if (time1.length === 0) {
-        return "";
-    }
-    for (let j = 0; j < time1.length; j++) {
-        if (A[time1[j]] == 2) {
-            for (let k = 0; k < time2.length; k++) {
-                if (A[time2[k]] <= 3 && time2[k] !== time1[j]) {
-                    time1 = `${A[j]}`;
-                    time2.replace(time1[j], "");
-                    time4.replace(time1[j], "");
-                    time5.replace(time1[j], "");
-                    break;
-                }
+    let newArray = A.slice();
+    let t1 = A.indexOf(2);
+    let t2 = -1;
+    let t3 = -1;
+    let t4 = -1;
+    if (t1 !== -1) {
+        t2 = A.indexOf(3);
+        if (t2 === -1) t2 = A.indexOf(2, t1 + 1);
+        if (t2 === -1) t2 = A.indexOf(1);
+        if (t2 === -1) t2 = A.indexOf(0);
+        if (t2 !== -1) {
+            let checkValidt3 = false;
+            for (let i = 0; i < A.length; i++) {
+                if (i === t1 || i === t2) continue;
+                if (A[i] < 6) checkValidt3 = true;
             }
-            break;
+            if (checkValidt3 === false) t2 = -1;
         }
-        else if ()
+        if (t2 === -1) t1 = -1;
     }
+    if (t1 === -1) {
+        t1 = A.indexOf(1);
+        if (t1 === -1) t1 = A.indexOf(0);
+        if (t1 === -1) return "";
+    }
+    newArray.splice(t1, 1);
+    if (t2 !== -1) {
+        let spliceIndex = newArray.indexOf(A[t2]);
+        newArray.splice(spliceIndex, 1);
+    }
+    if (t2 === -1) {
+        let maxNum = Math.max(...newArray);
+        t2 = A.indexOf(maxNum);
+        newArray.splice(newArray.indexOf(maxNum), 1);
+    }
+
+    for (let i = 5 ; i >= 0; i--) {
+        t3 = newArray.indexOf(i);
+        if (t3 !== -1) break;
+    }
+    if (t3 === -1) return "";
+
+    t3 = A.indexOf(newArray[t3]);
+    let spliceIndex = newArray.indexOf(A[t3]);
+    newArray.splice(spliceIndex, 1);
+    t4 = newArray[0];
+    t1 = A[t1];
+    t2= A[t2];
+    t3 = A[t3];
+    return `${t1}${t2}:${t3}${t4}`;
 };
+
+console.log(largestTimeFromDigits([0,0,0,2]))
