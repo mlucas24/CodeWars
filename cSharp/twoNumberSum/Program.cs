2426cec0d748c9e8085b12a2b5456e6da135c961
mlucas24 <mlucas24@icloud.com>
2020-12-03 17:39:07 -0500

    added TwoNumberSum

---
+++ cSharp/twoNumberSum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program {
+	public static int[] TwoNumberSum(int[] array, int targetSum) {
+		// Write your code here.
+		for(int i = 0; i < array.Length; i++) 
+		{
+			int searchNum = targetSum - array[i];
+			if (Array.IndexOf(array, searchNum, i+1) != -1)
+			{
+				return new int[2] {array[i], searchNum};
+			}
+		}
+		return new int[0];
+	}
+}
