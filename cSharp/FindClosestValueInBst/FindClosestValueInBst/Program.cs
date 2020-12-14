278d13a6c224f3b5688ea29aa80b6302bfd573c7
Matthew Lucas <matthewlucas@Matthews-MacBook-Pro.local>
2020-12-14 12:24:41 -0500

    added FindClosestValueInBst

---
+++ cSharp/FindClosestValueInBst/FindClosestValueInBst/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Program
+{
+	public static int FindClosestValueInBst(BST tree, int target)
+	{
+		return FindClosestValueInBst(tree, target, tree.value);
+    }
+	public static int FindClosestValueInBst(BST tree, int target, int closest)
+    {
+		if (Math.Abs(target - tree.value) < Math.Abs(closest - target))
+        {
+			closest = tree.value;
+        }
+		if (tree.value > target && tree.left != null)
+		{
+			return FindClosestValueInBst(tree.left, target, closest);
+		}
+		else if (tree.value < target && tree.right != null)
+		{
+			return FindClosestValueInBst(tree.right, target, closest);
+		}
+		else return closest;
+    }
+
+	public class BST
+	{
+		public int value;
+		public BST left;
+		public BST right;
+
+		public BST(int value)
+		{
+			this.value = value;
+		}
+	}
+
+	public static void Main()
+    {
+        Console.WriteLine("hello");
+    }
+}
+
+
---
+++ cSharp/FindClosestValueInBst/FindClosestValueInBstTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FindClosestValueInBstTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+			//arrange
+			var root = new Program.BST(10);
+			root.left = new Program.BST(5);
+			root.left.left = new Program.BST(2);
+			root.left.left.left = new Program.BST(1);
+			root.left.right = new Program.BST(5);
+			root.right = new Program.BST(15);
+			root.right.left = new Program.BST(13);
+			root.right.left.right = new Program.BST(14);
+			root.right.right = new Program.BST(22);
+            int expected = 13;
+
+            //Act
+            var actual = Program.FindClosestValueInBst(root, 12);
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+    }
+}
